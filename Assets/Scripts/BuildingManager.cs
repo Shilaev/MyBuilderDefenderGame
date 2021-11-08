fb727159e8ddf5ae78d426de6a31a87fafddcc70
Shilaev Valentin <valentin.shilaev@yandex.ru>
2021-11-08 13:46:33 +0300

    + meta + buildmanager test code

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BuildingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera _camera;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var mousePos      = Mouse.current.position.ReadValue();
+        var mouseWorldPos = _camera.ScreenToWorldPoint(mousePos);
     }
 }
