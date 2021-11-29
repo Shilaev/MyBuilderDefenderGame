72556a377498bdbd96e5ed566fb6162d8076d0d9
Shilaev Valentin <valentin.shilaev@yandex.ru>
2021-11-29 22:48:22 +0300

    + test scene

--- Assets/Scripts/BuildingTypeSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "GameObjects/BuildingType")]
-public class BuildingTypeSO : ScriptableObject
-{
-    public string    name;
-    public Transform prefab;
-}
