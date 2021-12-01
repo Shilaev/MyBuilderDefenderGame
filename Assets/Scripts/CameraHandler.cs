d8381782233b0a07d46d7643f5dd34c61b3147ce
Shilaev Valentin <valentin.shilaev@yandex.ru>
2021-12-01 08:34:10 +0300

    setup camera zoom speed

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -1,18 +1,22 @@
 using System;
 using Cinemachine;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraHandler : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _cinemachineVirtualCamera;
-
-    [SerializeField] float _moveSpeed = 15f;
-    [SerializeField] private float _zoomAmount = .8f;
+    [SerializeField] private float _moveSpeed = 15f;
+    [SerializeField] private float _minZoom = 2;
+    [SerializeField] private float _maxZoom = 10;
+    [SerializeField] private float _zoomSpeed;
     private float _orthographicSize;
+    private float _targetOrthographicSize;
 
     private void Start()
     {
         _orthographicSize = _cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        _targetOrthographicSize = _orthographicSize;
     }
 
     private void Update()
@@ -23,7 +27,12 @@ public class CameraHandler : MonoBehaviour
         Vector3 moveDirection = new Vector2(xAxis, yAxis);
         transform.position += moveDirection * _moveSpeed * Time.deltaTime;
 
-        _orthographicSize -= Input.mouseScrollDelta.y * _zoomAmount;
+        _targetOrthographicSize -= Input.mouseScrollDelta.y;
+        _targetOrthographicSize = math.clamp(_targetOrthographicSize, _minZoom, _maxZoom);
+
+        _zoomSpeed = 10f;
+        _orthographicSize = math.lerp(_orthographicSize, _targetOrthographicSize, Time.deltaTime * _zoomSpeed);
+
         _cinemachineVirtualCamera.m_Lens.OrthographicSize = _orthographicSize;
     }
 }
