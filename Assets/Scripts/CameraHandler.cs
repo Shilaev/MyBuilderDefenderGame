525e2c6488df9afb5aabc551296a468c2cf5e169
Shilaev Valentin <valentin.shilaev@yandex.ru>
2021-12-01 08:20:34 +0300

    setup zoom amount

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -7,6 +7,7 @@ public class CameraHandler : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera _cinemachineVirtualCamera;
 
     [SerializeField] float _moveSpeed = 15f;
+    [SerializeField] private float _zoomAmount = .8f;
     private float _orthographicSize;
 
     private void Start()
@@ -22,7 +23,7 @@ public class CameraHandler : MonoBehaviour
         Vector3 moveDirection = new Vector2(xAxis, yAxis);
         transform.position += moveDirection * _moveSpeed * Time.deltaTime;
 
-        _orthographicSize -= Input.mouseScrollDelta.y;
+        _orthographicSize -= Input.mouseScrollDelta.y * _zoomAmount;
         _cinemachineVirtualCamera.m_Lens.OrthographicSize = _orthographicSize;
     }
 }
