211d0d0ebb1b0d6b39212a405dd9518986603105
Shilaev Valentin <valentin.shilaev@yandex.ru>
2021-12-15 17:23:37 +0300

    resource amount functional

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceGenerator : MonoBehaviour
 {
+    private List<BuildingTypeSO> _buildings;
     private BuildingTypeSO _buildingType;
     private float _timer;
     private float _timerMax;
-    private List<BuildingTypeSO> _buildings;
 
     private void Awake()
     {
@@ -16,10 +15,39 @@ public class ResourceGenerator : MonoBehaviour
         _buildings = GetComponent<BuildingTypeHolder>().buildingType;
     }
 
+    private void Start()
+    {
+        var collider2DArray =
+            Physics2D.OverlapCircleAll(transform.position,
+                _buildingType.resourceGeneratorData.resourceDetectionRadius);
+
+        var nearbyResourceAmount = 0;
+        foreach (var collider2D in collider2DArray)
+        {
+            var resourceNode = collider2D.GetComponent<ResourceNode>();
+            if (resourceNode != null)
+                // it's a resource node!
+                nearbyResourceAmount++;
+        }
+
+        nearbyResourceAmount = Mathf.Clamp(nearbyResourceAmount, 0,
+            _buildingType.resourceGeneratorData.maxResourceAmount);
+
+        if (nearbyResourceAmount == 0)
+            // No resource nodes nearby
+            // Disable resource generator
+            enabled = false;
+        else
+            _timerMax = _buildingType.resourceGeneratorData.timeMax / 2f +
+                        (1 - _buildingType.resourceGeneratorData.timeMax *
+                            (nearbyResourceAmount / _buildingType.resourceGeneratorData.maxResourceAmount));
+
+        Debug.Log($"nearbyResourceAmount: {nearbyResourceAmount}, timeMax: {_timerMax}");
+    }
+
     private void Update()
     {
-
-        foreach (BuildingTypeSO building in _buildings)
+        foreach (var building in _buildings)
         {
             _buildingType = building;
             _timerMax = _buildingType.resourceGeneratorData.timeMax;
@@ -32,35 +60,5 @@ public class ResourceGenerator : MonoBehaviour
                 ResourceManager.Instance.AddResource(_buildingType.resourceGeneratorData.resourceType, 1);
             }
         }
-    }
-
-    private void Start()
-    {
-        Collider2D[] collider2DArray =
-                Physics2D.OverlapCircleAll(transform.position,
-                                           _buildingType.resourceGeneratorData.resourceDetectionRadius);
-
-        int nearbyResourceAmount = 0;
-        foreach (Collider2D collider2D in collider2DArray)
-        {
-            ResourceNode resourceNode = collider2D.GetComponent<ResourceNode>();
-            if (resourceNode != null)
-            {
-                // it's a resource node!
-                nearbyResourceAmount++;
-            }
-        }
-
-        nearbyResourceAmount = Mathf.Clamp(nearbyResourceAmount, 0,
-                                           _buildingType.resourceGeneratorData.maxResourceAmount);
-
-        if (nearbyResourceAmount == 0)
-        {
-            // No resource nodes nearby
-            // Disable resource generator
-            enabled = false;
-        }
-
-        Debug.Log($"nearbyResourceAmount: {nearbyResourceAmount}");
     }
 }
